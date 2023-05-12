c003e795880a7463d767864c00cb1c3b4b2b22fc
IKNOWHOW\kdiamantis <kdiamantis@iknowhow.com>
2023-05-12 13:16:52 +0300

    Login page with error in case of wrong username/password

---
+++ src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+
+        //just returns the name of the html file that will be loaded
+        //i have to create manually this file
+        return "plain-login";
+    }
+}
--- src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,7 +33,25 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-        
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and()
+                .httpBasic();
+
+
+        return http.build();
     }
 
 }
